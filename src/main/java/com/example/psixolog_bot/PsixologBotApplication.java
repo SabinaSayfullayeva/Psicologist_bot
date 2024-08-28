00495f1d95d006d1c0f7170ffb4f7b3ec7b18688
SabinaSayfullayeva <ssayfullayeva50@gmail.com>
2024-08-28 08:16:56 +0300

    initial commit

---
+++ src/main/java/com/example/psixolog_bot/PsixologBotApplication.java
@@ -0,0 +1,14 @@
+package com.example.psixolog_bot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsixologBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsixologBotApplication.class, args);
+    }
+
+}
